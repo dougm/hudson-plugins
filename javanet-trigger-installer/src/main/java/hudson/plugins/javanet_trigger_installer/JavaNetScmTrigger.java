689a43ad47821660f40a8ab777a3e6a2d5c7ba8b
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2006-11-12 22:54:36 +0000

    change in the configuration now updates java.net.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@1110 71c3de6d-444a-0410-be80-ed276b4c234a

--- javanet-trigger-installer/src/main/java/hudson/plugins/javanet_trigger_installer/JavaNetScmTrigger.java
+++ javanet-trigger-installer/src/main/java/hudson/plugins/javanet_trigger_installer/JavaNetScmTrigger.java
@@ -1,6 +1,8 @@
 package hudson.plugins.javanet_trigger_installer;
 
 import hudson.model.Descriptor;
+import hudson.model.Project;
+import hudson.plugins.javanet_trigger_installer.Task.Update;
 import hudson.triggers.Trigger;
 import org.kohsuke.stapler.StaplerRequest;
 
@@ -13,6 +15,18 @@ public class JavaNetScmTrigger extends Trigger {
 
     public Descriptor<Trigger> getDescriptor() {
         return DESCRIPTOR;
+    }
+
+
+    public void start(Project project, boolean newInstance) {
+        super.start(project, newInstance);
+        if(newInstance)
+            new Update(project).scheduleHighPriority();
+    }
+
+    public void stop() {
+        super.stop();
+        new Update(project).scheduleHighPriority();
     }
 
     public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
