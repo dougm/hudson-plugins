2684550e954b2424b01f89877207e97691be033d
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2006-12-20 17:03:04 +0000

    should run before the notifiers.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@1391 71c3de6d-444a-0410-be80-ed276b4c234a

--- javatest-report/src/main/java/hudson/plugins/javatest_report/PluginImpl.java
+++ javatest-report/src/main/java/hudson/plugins/javatest_report/PluginImpl.java
@@ -9,6 +9,6 @@ import hudson.tasks.BuildStep;
  */
 public class PluginImpl extends Plugin {
     public void start() throws Exception {
-        BuildStep.PUBLISHERS.add(JavaTestReportPublisher.DESCRIPTOR);
+        BuildStep.PUBLISHERS.add(0,JavaTestReportPublisher.DESCRIPTOR);
     }
 }
