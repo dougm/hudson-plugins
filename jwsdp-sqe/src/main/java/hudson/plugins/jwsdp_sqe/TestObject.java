35b8eb6862caabcd63ab5ae7d4251170d22f2ba7
ramapulavarthi <ramapulavarthi@71c3de6d-444a-0410-be80-ed276b4c234a>
2007-01-09 22:22:17 +0000

    Adding result trend for tests and testcases
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@1726 71c3de6d-444a-0410-be80-ed276b4c234a

--- jwsdp-sqe/src/main/java/hudson/plugins/jwsdp_sqe/TestObject.java
+++ jwsdp-sqe/src/main/java/hudson/plugins/jwsdp_sqe/TestObject.java
@@ -2,6 +2,25 @@ package hudson.plugins.jwsdp_sqe;
 
 import hudson.model.Build;
 import hudson.model.ModelObject;
+import hudson.util.ChartUtil;
+import hudson.util.DataSetBuilder;
+import hudson.util.ShiftedCategoryAxis;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.renderer.category.AreaRenderer;
+import org.jfree.chart.renderer.AreaRendererEndType;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.ui.RectangleInsets;
+
+import java.io.IOException;
+import java.awt.*;
 
 /**
  * Common data applicable to all test model objects.
@@ -119,4 +138,109 @@ public abstract class TestObject<S extends TestObject<S>>
 
     public abstract int getTotalCount();
     public abstract int getFailCount();
+
+    /**
+     * Generates a PNG image for the test result trend.
+     */
+    public void doTestTrendGraph( StaplerRequest req, StaplerResponse rsp) throws IOException {
+        if(ChartUtil.awtProblem) {
+            // not available. send out error message
+            rsp.sendRedirect2(req.getContextPath()+"/images/headless.png");
+            return;
+        }
+
+        if(req.checkIfModified(getOwner().getTimestamp(),rsp))
+            return;
+
+        class BuildLabel implements Comparable<BuildLabel> {
+            private final Build build;
+
+            public BuildLabel(Build build) {
+                this.build = build;
+            }
+
+            public int compareTo(BuildLabel that) {
+                return this.build.number-that.build.number;
+            }
+
+            public boolean equals(Object o) {
+                BuildLabel that = (BuildLabel) o;
+                return build==that.build;
+            }
+
+            public int hashCode() {
+                return build.hashCode();
+            }
+
+            public String toString() {
+                return build.getDisplayName();
+            }
+        }
+
+        boolean failureOnly = Boolean.valueOf(req.getParameter("failureOnly"));
+
+        DataSetBuilder<String,BuildLabel> dsb = new DataSetBuilder<String,BuildLabel>();
+
+        for(TestObject a=this; a!=null; a=a.getPreviousResult() ) {
+            dsb.add( a.getFailCount(), "failed", new BuildLabel(a.getOwner()));
+            if(!failureOnly)
+                dsb.add( a.getTotalCount()-a.getFailCount(),"total", new BuildLabel(a.getOwner()));
+        }
+
+        ChartUtil.generateGraph(req,rsp,createChart(dsb.build()),500,200);
+    }
+
+    private JFreeChart createChart(CategoryDataset dataset) {
+
+        final JFreeChart chart = ChartFactory.createStackedAreaChart(
+            null,                   // chart title
+            null,                   // unused
+            "count",                  // range axis label
+            dataset,                  // data
+            PlotOrientation.VERTICAL, // orientation
+            false,                     // include legend
+            true,                     // tooltips
+            false                     // urls
+        );
+
+        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+
+        // set the background color for the chart...
+
+//        final StandardLegend legend = (StandardLegend) chart.getLegend();
+//        legend.setAnchor(StandardLegend.SOUTH);
+
+        chart.setBackgroundPaint(Color.white);
+
+        final CategoryPlot plot = chart.getCategoryPlot();
+
+        // plot.setAxisOffset(new Spacer(Spacer.ABSOLUTE, 5.0, 5.0, 5.0, 5.0));
+        plot.setBackgroundPaint(Color.WHITE);
+        plot.setOutlinePaint(null);
+        plot.setForegroundAlpha(0.8f);
+//        plot.setDomainGridlinesVisible(true);
+//        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinesVisible(true);
+        plot.setRangeGridlinePaint(Color.black);
+
+        CategoryAxis domainAxis = new ShiftedCategoryAxis(null);
+        plot.setDomainAxis(domainAxis);
+        domainAxis.setCategoryLabelPositions(CategoryLabelPositions.UP_90);
+        domainAxis.setLowerMargin(0.0);
+        domainAxis.setUpperMargin(0.0);
+        domainAxis.setCategoryMargin(0.0);
+
+        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+        AreaRenderer ar = (AreaRenderer) plot.getRenderer();
+        ar.setEndType(AreaRendererEndType.TRUNCATE);
+        ar.setSeriesPaint(0,new Color(0xEF,0x29,0x29));
+        ar.setSeriesPaint(1,new Color(0x72,0x9F,0xCF));
+
+        // crop extra space around the graph
+        plot.setInsets(new RectangleInsets(0,0,0,5.0));
+
+        return chart;
+    }
 }
