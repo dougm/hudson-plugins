5cbc3a7cfd196556bc0d022f83f7ec5f6a219509
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2008-03-06 07:47:08 +0000

    ready for a release
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@7357 71c3de6d-444a-0410-be80-ed276b4c234a

--- javanet/src/main/java/hudson/plugins/javanet/PluginImpl.java
+++ javanet/src/main/java/hudson/plugins/javanet/PluginImpl.java
@@ -8,6 +8,10 @@ import hudson.model.listeners.ItemListener;
 import hudson.triggers.SafeTimerTask;
 import hudson.triggers.Trigger;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.io.IOException;
+
 /**
  * @author Kohsuke Kawaguchi
  */
@@ -17,11 +21,23 @@ public class PluginImpl extends Plugin {
 
         Hudson.getInstance().getJobListeners().add(new ItemListener() {
             public void onLoaded() {
+                // when we are installed for the first time, hook this up to all existing jobs
+                // so that this can be seen w/o reconfiguration.
+                for(AbstractProject<?,?> j : Hudson.getInstance().getAllItems(AbstractProject.class)) {
+                    StatsProperty p = j.getProperty(StatsProperty.class);
+                    if(p==null)
+                        try {
+                            j.addProperty(new StatsProperty());
+                        } catch (IOException e) {
+                            LOGGER.log(Level.SEVERE, "Failed to persist "+j,e);
+                        }
+                }
+
                 Trigger.timer.scheduleAtFixedRate(new SafeTimerTask() {
                     protected void doRun() {
                         for(AbstractProject<?,?> j : Hudson.getInstance().getAllItems(AbstractProject.class)) {
                             StatsProperty p = j.getProperty(StatsProperty.class);
-                            if(p==null) continue; // TODO: add this property after the fact
+                            if(p==null) continue;
                             JavaNetStatsAction a = p.getJobAction(j);
                             if(a==null)  continue;
 
@@ -36,4 +52,6 @@ public class PluginImpl extends Plugin {
     static final long MINUTE = 1000*60;
     static final long HOUR   = 24*60*MINUTE;
     static final long DAY   = 24*HOUR;
+
+    private static final Logger LOGGER = Logger.getLogger(PluginImpl.class.getName());
 }
