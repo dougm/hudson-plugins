d4eb565db73fddadf33f2c4762e15e6c3f7cc6c7
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-04-16 01:03:27 +0000

    Modified to pick up the netboot kernel image from Ubuntu CD/DVD. Asking the user to provide a CD is useful for multiple reasons, such as automatic preseeding for the DVD over HTTP, ability for users to use Ubuntu-derivative distributions, and so on.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@17200 71c3de6d-444a-0410-be80-ed276b4c234a

--- pxe-ubuntu/src/main/java/hudson/plugins/pxe/ubuntu/UbuntuBootConfiguration.java
+++ pxe-ubuntu/src/main/java/hudson/plugins/pxe/ubuntu/UbuntuBootConfiguration.java
@@ -2,17 +2,28 @@ package hudson.plugins.pxe.ubuntu;
 
 import hudson.Extension;
 import hudson.Util;
+import hudson.model.Hudson;
 import hudson.plugins.pxe.BootConfiguration;
 import hudson.plugins.pxe.BootConfigurationDescriptor;
-import org.kohsuke.stapler.DataBoundConstructor;
-import org.jvnet.hudson.tftpd.Data;
+import hudson.util.FormValidation;
+import static hudson.util.FormValidation.error;
+import static hudson.util.FormValidation.ok;
 import org.apache.commons.io.IOUtils;
+import org.jvnet.hudson.tftpd.Data;
+import org.kohsuke.loopy.FileEntry;
+import org.kohsuke.loopy.iso9660.ISO9660FileSystem;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Collections;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Ubuntu boot configuration.
@@ -20,38 +31,99 @@ import java.util.regex.Matcher;
  * @author Kohsuke Kawaguchi
  */
 public class UbuntuBootConfiguration extends BootConfiguration {
-    public final Release release;
-
-    public UbuntuBootConfiguration(Release release) {
-        this.release = release;
-    }
+    /**
+     * Location of the CD/DVD image file.
+     */
+    public final File iso;
 
     @DataBoundConstructor
-    public UbuntuBootConfiguration(String release) {
-        this.release = Release.get(release);
+    public UbuntuBootConfiguration(File iso) {
+        this.iso = iso;
     }
 
     public String getPxeLinuxConfigFragment() throws IOException {
         return String.format("LABEL %1$s\n" +
-                "    MENU LABEL Ubuntu %2$s (%3$s)\n" +
+                "    MENU LABEL %2$s\n" +
                 "    KERNEL vesamenu.c32\n" +
-                "    APPEND %4$s/menu.txt \n",
-                hashCode(), release.number, release.nickName, hashCode());
+                "    APPEND %1$s/menu.txt \n",
+                getId(), getRelease());
+    }
+
+    /**
+     * This returns string like "Ubuntu-Server 8.10 "Intrepid Ibex" - Release i386 (20081028.1)"
+     */
+    public String getRelease() throws IOException {
+        return getReleaseInfo(iso);
     }
 
     /**
      * Serves menu.txt by replacing variables.
      */
     public Data tftp(String fileName) throws IOException {
-        Matcher m = MENU_CONFIG.matcher(fileName);
-        if(!m.matches())    return null;
-        if(!m.group(1).equals(String.valueOf(hashCode())))   return null;
+        String prefix = getId() + "/";
+        if(!fileName.startsWith(prefix))   return null;    // not ours
 
-        String template = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("tftp/ubuntu/menu.txt"));
-        return Data.from(Util.replaceMacro(template, Collections.singletonMap("RELEASE",release.nickName)));
+        fileName=fileName.substring(prefix.length());
+
+        if(fileName.equals("menu.txt")) {
+            // menu
+            String template = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("tftp/ubuntu/menu.txt"));
+            Map<String,String> props = new HashMap<String, String>();
+            props.put("RELEASE",getRelease());
+            props.put("ID",getId());
+
+            return Data.from(Util.replaceMacro(template,props));
+        }
+
+        if(fileName.equals("linux") || fileName.equals("initrd.gz")) {
+            ISO9660FileSystem fs = new ISO9660FileSystem(iso,false);
+            FileEntry installer = fs.get("/install/netboot/ubuntu-installer");
+            if(installer==null) throw new IOException("/install/netboot/ubuntu-installer not found on "+iso);
+            LinkedHashMap<String,FileEntry> children = installer.childEntries();
+            FileEntry arch = children.get("i386");
+            if(arch==null)  arch=children.get("amd64");
+            if(arch==null)      throw new IOException("/install/netboot/ubuntu-installer/(amd64|i386) not found on "+iso);
+
+            final FileEntry data = arch.grab(fileName);
+            return new Data() {
+                public InputStream read() throws IOException {
+                    return data.read();
+                }
+
+                @Override
+                public int size() throws IOException {
+                    return data.getSize();
+                }
+            };
+        }
+
+        return null;
     }
 
-    private static final Pattern MENU_CONFIG = Pattern.compile("(.+)/menu.txt");
+    private static String getReleaseInfo(File iso) throws IOException {
+        ISO9660FileSystem fs=null;
+        try {
+            try {
+                fs = new ISO9660FileSystem(iso,false);
+            } catch (IOException e) {
+                LOGGER.log(Level.INFO,iso+" isn't an ISO file?",e);
+                throw error(iso+" doesn't look like an ISO file");
+            }
+
+            FileEntry info = fs.get("/.disk/info");
+            if(info==null)
+                throw error(iso+" doesn't look like an Ubuntu CD/DVD image");
+
+            FileEntry installer = fs.get("/install/netboot/ubuntu-installer");
+            if(installer==null)
+                throw error(iso+" doesn't have the network boot installer in it. Perhaps it's a desktop CD?");
+
+            return IOUtils.toString(info.read());
+        } finally {
+            if(fs!=null)
+                fs.close();
+        }
+    }
 
     @Extension
     public static class DescriptorImpl extends BootConfigurationDescriptor {
@@ -59,8 +131,25 @@ public class UbuntuBootConfiguration extends BootConfiguration {
             return "Ubuntu";
         }
 
+        public FormValidation doCheckIso(@QueryParameter String value) throws IOException {
+            // insufficient permission to perform validation?
+            if(!Hudson.getInstance().hasPermission(Hudson.ADMINISTER)) return ok();
+
+            File f = new File(value);
+            if(!f.exists())
+                return error("No such file file exists: "+value);
+
+            try {
+                return ok(getReleaseInfo(f));
+            } catch (FormValidation e) {
+                return e;
+            }
+        }
+
         public List<Release> getReleases() {
             return Release.RELEASES;
         }
     }
+
+    private static final Logger LOGGER = Logger.getLogger(UbuntuBootConfiguration.class.getName());
 }
