bbc1ea24941400bfdda7bb22832080e792ae0c4a
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-05-18 17:50:21 +0000

    allow authentication to be limited to a subnet.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@18181 71c3de6d-444a-0410-be80-ed276b4c234a

--- source-ip-realm/src/main/java/hudson/plugins/sourceip/SourceIpSecurityRealm.java
+++ source-ip-realm/src/main/java/hudson/plugins/sourceip/SourceIpSecurityRealm.java
@@ -1,21 +1,25 @@
 package hudson.plugins.sourceip;
 
 import hudson.Extension;
+import hudson.Util;
+import static hudson.Util.fixEmptyAndTrim;
 import hudson.model.Descriptor;
-import hudson.security.SecurityRealm;
 import hudson.security.GroupDetails;
+import hudson.security.SecurityRealm;
+import hudson.util.FormValidation;
 import org.acegisecurity.Authentication;
 import org.acegisecurity.AuthenticationCredentialsNotFoundException;
 import org.acegisecurity.AuthenticationException;
 import org.acegisecurity.AuthenticationManager;
 import org.acegisecurity.GrantedAuthority;
 import org.acegisecurity.GrantedAuthorityImpl;
-import org.acegisecurity.userdetails.UserDetailsService;
-import org.acegisecurity.userdetails.UserDetails;
-import org.acegisecurity.userdetails.UsernameNotFoundException;
-import org.acegisecurity.userdetails.User;
 import org.acegisecurity.context.SecurityContextHolder;
 import org.acegisecurity.providers.UsernamePasswordAuthenticationToken;
+import org.acegisecurity.providers.anonymous.AnonymousAuthenticationToken;
+import org.acegisecurity.userdetails.User;
+import org.acegisecurity.userdetails.UserDetails;
+import org.acegisecurity.userdetails.UserDetailsService;
+import org.acegisecurity.userdetails.UsernameNotFoundException;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.springframework.dao.DataAccessException;
 
@@ -26,6 +30,9 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -36,10 +43,17 @@ import java.util.regex.Pattern;
  * @author Kohsuke Kawaguchi
  */
 public class SourceIpSecurityRealm extends SecurityRealm {
-    @DataBoundConstructor
-    public SourceIpSecurityRealm() {
-    }
+    /**
+     * Represents the subnet that gets authenticated.
+     * String like "192.168.1.0/255.255.255.0"
+     */
+    public final String pattern;
 
+    @DataBoundConstructor
+    public SourceIpSecurityRealm(String pattern) throws FormValidation {
+        DescriptorImpl.decomposePattern(pattern);  // error check
+        this.pattern = pattern;
+    }
 
     /**
      * The incoming request is "authenticated" automatically as an user that represents the remote IP address.
@@ -54,16 +68,33 @@ public class SourceIpSecurityRealm extends SecurityRealm {
             }
 
             public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-                String addr = request.getRemoteAddr();
+                Authentication auth = createAuth(request);
 
-                SecurityContextHolder.getContext().setAuthentication(
-                        new UsernamePasswordAuthenticationToken(addr,addr, buildGroupList(addr)));
+                SecurityContextHolder.getContext().setAuthentication(auth);
                 chain.doFilter(request,response);
             }
 
             public void destroy() {
             }
+
+            private Authentication createAuth(ServletRequest request) throws FormValidation, UnknownHostException {
+                String addr = request.getRemoteAddr();
+
+                Inet4Address[] adrs = DescriptorImpl.decomposePattern(pattern);
+                Inet4Address user = (Inet4Address) InetAddress.getByName(addr);
+
+                int mask = toInt(adrs[1]);
+                if((toInt(user)&mask)==(toInt(adrs[0])&mask))
+                    return new UsernamePasswordAuthenticationToken(addr, addr, buildGroupList(addr));
+                else
+                    return new AnonymousAuthenticationToken("anonymous","anonymous",new GrantedAuthority[]{new GrantedAuthorityImpl("anonymous")});
+            }
         };
+    }
+
+    private int toInt(Inet4Address a) {
+        byte[] x = a.getAddress();
+        return (x[0]<<24)|((x[1]<<16)&0x00FF0000)|((x[2]<<8)&0x0000FF00)|((x[3])&0x000000FF);
     }
 
     /**
@@ -145,6 +176,25 @@ public class SourceIpSecurityRealm extends SecurityRealm {
     public static class DescriptorImpl extends Descriptor<SecurityRealm> {
         public String getDisplayName() {
             return "By the IP address of the user";
+        }
+
+        public static Inet4Address[] decomposePattern(String p) throws FormValidation {
+            try {
+                if(fixEmptyAndTrim(p)==null) // if empty, authenticate everyone
+                    return new Inet4Address[] {
+                            (Inet4Address)InetAddress.getByName("0.0.0.0"),
+                            (Inet4Address)InetAddress.getByName("0.0.0.0")};
+
+                String[] tokens = p.split("/");
+                if(tokens.length!=2 || !IP.matcher(tokens[0]).matches() || !IP.matcher(tokens[1]).matches())
+                    throw FormValidation.error("Expecting a string like 192.168.1.0/255.255.255.0");
+    
+                return new Inet4Address[] {
+                        (Inet4Address)InetAddress.getByName(tokens[0]),
+                        (Inet4Address)InetAddress.getByName(tokens[1])};
+            } catch (UnknownHostException e) {
+                throw FormValidation.error("Expecting a string like 192.168.1.0/255.255.255.0: "+e.getMessage());
+            }
         }
     }
 }
