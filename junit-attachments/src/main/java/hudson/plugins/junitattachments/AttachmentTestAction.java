5f3750f301b662836af08517cc57b83d64732ea8
huybrechts <huybrechts@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-08-09 20:35:50 +0000

    Merging in the junit branch. Lots of new features and bugfixes, including: - support for TestAction that can contribute various things to any TestObject (packages, classes and methods), using the TestDataPublisher extension point. Features include UI on the TestObject page, badges on the overview page, tasks in the side panel and annotating test output. - a junit-attachments plugin that uses this to record test attachments (files) - an extension to the claim plugin that offers claims on failing tests - editable test descriptions (on any TestObject) - a history page for all TestObjects (including test count and duration graph) - error details for failed tests are shown in a collapsed panel in test overview pages (package+class) - [FIXED HUDSON-3149] if file is .xml but does not parse, record a fake failing test - [FIXED HUDSON-1820] extra column displaying skipped tests - [HUDSON-2046] new test packages, classes and methods are shown in bold - [FIXED HUDSON-2228] test duration charts on all levels - [FIXED HUDSON-3451] expand environment variables in test results path
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@20580 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ junit-attachments/src/main/java/hudson/plugins/junitattachments/AttachmentTestAction.java
@@ -0,0 +1,60 @@
+package hudson.plugins.junitattachments;
+
+import hudson.FilePath;
+import hudson.model.DirectoryBrowserSupport;
+import hudson.model.Hudson;
+import hudson.tasks.junit.TestAction;
+import hudson.tasks.junit.TestObject;
+
+import java.util.List;
+
+public class AttachmentTestAction extends TestAction {
+	
+	private final FilePath storage;
+	private final List<String> attachments;
+	private final TestObject testObject;
+
+	public AttachmentTestAction(TestObject testObject, FilePath storage, List<String> attachments) {
+		this.storage = storage;
+		this.testObject = testObject;
+		this.attachments = attachments;
+	}
+
+	public String getDisplayName() {
+		return "Attachments";
+	}
+
+	public String getIconFileName() {
+		return "package.gif";
+	}
+
+	public String getUrlName() {
+		return "attachments";
+	}
+	
+	public DirectoryBrowserSupport doDynamic() {
+		return new DirectoryBrowserSupport(this, storage, "Attachments", "package.gif", true);
+	}
+
+	@Override
+	public String annotate(String text) {
+		String url = Hudson.getInstance().getRootUrl()
+				+ testObject.getOwner().getUrl() + "testReport"
+				+ testObject.getUrl() + "/attachments/";
+		for (String attachment : attachments) {
+			text = text.replace(attachment, "<a href=\"" + url + attachment
+					+ "\">" + attachment + "</a>");
+		}
+		return text;
+	}
+
+	public List<String> getAttachments() {
+		return attachments;
+	}
+
+	public TestObject getTestObject() {
+		return testObject;
+	}
+
+
+}
