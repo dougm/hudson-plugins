4574f64f540c8dab31e601a345d52d39c40145fe
mindless <mindless@71c3de6d-444a-0410-be80-ed276b4c234a>
2009-10-22 05:53:42 +0000

    [jmx-invoker] Update uses of deprecated APIs
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@23013 71c3de6d-444a-0410-be80-ed276b4c234a

--- jmx-invoker/src/main/java/com/mtvi/plateng/hudson/JMXInvokerPublisher.java
+++ jmx-invoker/src/main/java/com/mtvi/plateng/hudson/JMXInvokerPublisher.java
@@ -3,14 +3,18 @@
  */
 package com.mtvi.plateng.hudson;
 
+import hudson.Extension;
 import static hudson.Util.fixEmpty;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
-import hudson.model.Descriptor;
 import hudson.model.Result;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
-import hudson.util.FormFieldValidator;
+import hudson.util.FormValidation;
 
 import java.io.IOException;
 import java.io.PrintStream;
@@ -22,14 +26,13 @@ import javax.management.ObjectName;
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
-import javax.servlet.ServletException;
 
 import net.sf.json.JSONObject;
 
 import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.StaplerResponse;
 
 /**
  * Hudson plugin that invokes a JMX operation upon a successful build.
@@ -37,9 +40,7 @@ import org.kohsuke.stapler.StaplerResponse;
  * @author edelsonj
  * @plugin
  */
-public class JMXInvokerPublisher extends Publisher {
-
-    public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+public class JMXInvokerPublisher extends Notifier {
 
     private JMXServiceURL mbeanServerUrl;
     private ObjectName objectName;
@@ -82,14 +83,6 @@ public class JMXInvokerPublisher extends Publisher {
         return new JMXServiceURL(value);
     }
 
-    /**
-     * @return
-     * @see hudson.model.Describable#getDescriptor()
-     */
-    public Descriptor<Publisher> getDescriptor() {
-        return DESCRIPTOR;
-    }
-
     public String getMbeanServerUrl() {
         return mbeanServerUrl.toString();
     }
@@ -108,6 +101,10 @@ public class JMXInvokerPublisher extends Publisher {
 
     public String getUsername() {
         return username;
+    }
+
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.STEP;
     }
 
     @Override
@@ -137,7 +134,8 @@ public class JMXInvokerPublisher extends Publisher {
         return true;
     }
 
-    public static final class DescriptorImpl extends Descriptor<Publisher> {
+    @Extension
+    public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
 
         public DescriptorImpl() {
             super(JMXInvokerPublisher.class);
@@ -147,85 +145,66 @@ public class JMXInvokerPublisher extends Publisher {
         /**
          * Performs on-the-fly validation of the mbean server url field.
          */
-        public void doCheckMBeanServerUrl(StaplerRequest request, StaplerResponse response)
-                throws IOException, ServletException {
-            new FormFieldValidator(request, response, false) {
-
-                @Override
-                protected void check() throws IOException, ServletException {
-                    String value = fixEmpty(request.getParameter("value"));
-                    if (value == null) {
-                        error("MBean Server URL cannot be empty");
-                    } else {
-                        JMXServiceURL url = null;
-                        try {
-                            url = createJMXServiceURL(value);
-                        } catch (MalformedURLException e) {
-                            error("Invalid MBean Server URL - %s", e.getMessage());
-                        }
-
-                        // JMXConnector jmxc = null;
-                        // try {
-                        // jmxc = JMXConnectorFactory.connect(url, null);
-                        // } catch (Exception e) {
-                        // error("Unable to connect to MBean Server: " +
-                        // e.getMessage());
-                        // } finally {
-                        // if (jmxc != null) {
-                        // jmxc.close();
-                        // }
-                        // }
-                    }
-
+        public FormValidation doCheckMBeanServerUrl(@QueryParameter String value) {
+            if (fixEmpty(value) == null) {
+                return FormValidation.error("MBean Server URL cannot be empty");
+            } else {
+                JMXServiceURL url = null;
+                try {
+                    url = createJMXServiceURL(value);
+                } catch (MalformedURLException e) {
+                    return FormValidation.error("Invalid MBean Server URL - %s", e.getMessage());
                 }
-            }.process();
+                return FormValidation.ok();
+
+                // JMXConnector jmxc = null;
+                // try {
+                // jmxc = JMXConnectorFactory.connect(url, null);
+                // } catch (Exception e) {
+                // error("Unable to connect to MBean Server: " +
+                // e.getMessage());
+                // } finally {
+                // if (jmxc != null) {
+                // jmxc.close();
+                // }
+                // }
+            }
         }
 
         /**
          * Performs on-the-fly validation of the object name field.
          */
-        public void doCheckObjectName(StaplerRequest request, StaplerResponse response)
-                throws IOException, ServletException {
-            new FormFieldValidator(request, response, false) {
-
-                @Override
-                protected void check() throws IOException, ServletException {
-                    String value = fixEmpty(request.getParameter("value"));
-                    if (value == null) {
-                        error("Object Name cannot be empty");
-                    } else {
-                        try {
-                            new ObjectName(value);
-                        } catch (Exception e) {
-                            error("Invalid object name: %s", e.getMessage());
-                        }
-                    }
-
+        public FormValidation doCheckObjectName(@QueryParameter String value) {
+            if (fixEmpty(value) == null) {
+                return FormValidation.error("Object Name cannot be empty");
+            } else {
+                try {
+                    new ObjectName(value);
+                } catch (Exception e) {
+                    return FormValidation.error("Invalid object name: %s", e.getMessage());
                 }
-            }.process();
+            }
+            return FormValidation.ok();
         }
 
         /**
          * Performs on-the-fly validation of the operation name field.
          */
-        public void doCheckOperationName(StaplerRequest request, StaplerResponse response)
-                throws IOException, ServletException {
-            new FormFieldValidator(request, response, false) {
-
-                @Override
-                protected void check() throws IOException, ServletException {
-                    String value = fixEmpty(request.getParameter("value"));
-                    if (value == null) {
-                        error("Operation Name cannot be empty");
-                    }
-
-                }
-            }.process();
+        public FormValidation doCheckOperationName(@QueryParameter String value) {
+            if (fixEmpty(value) == null) {
+                return FormValidation.error("Operation Name cannot be empty");
+            }
+            return FormValidation.ok();
         }
 
         @Override
         public String getDisplayName() {
             return "Invoke a JMX Operation";
+        }
+
+        @Override
+        public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+            return true;
         }
 
         @Override
