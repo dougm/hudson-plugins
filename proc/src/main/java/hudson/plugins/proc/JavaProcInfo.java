84611f34f854454046e491dbf56119d7262762dc
jitu <jitu@71c3de6d-444a-0410-be80-ed276b4c234a>
2010-01-15 01:27:51 +0000

    Changes so that the closures are run properly on the slave machines. If tools.jar is not loaded, it will be added to the correct classloader
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@25838 71c3de6d-444a-0410-be80-ed276b4c234a

--- proc/src/main/java/hudson/plugins/proc/JavaProcInfo.java
+++ proc/src/main/java/hudson/plugins/proc/JavaProcInfo.java
@@ -12,8 +12,10 @@ import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.io.File;
 import java.lang.management.ManagementFactory;
-import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -35,16 +37,20 @@ public class JavaProcInfo extends ProcInfo {
         return run.getBuiltOn().getChannel().call(new PropertiesTask(""+proc.getPid()));
     }
 
+    // returns the java stack for the process
+    public List<String> jstack() throws Exception {
+        return run.getBuiltOn().getChannel().call(new JstackTask(""+proc.getPid()));
+    }
+
     // Keep it static inner class, otherwise JavaProcInfo needs to be
     // specified as Serializable
-    private static class PropertiesTask implements Callable<Properties, Exception> {
-        private final String pid;
-
+    private static class PropertiesTask extends RemoteTask<Properties> {
         PropertiesTask(String pid) {
-            this.pid = pid;
+            super(pid);
         }
 
         public Properties call() throws Exception {
+            super.call();
             VirtualMachine vm = null;
             try {
                 vm = VirtualMachine.attach(pid);
@@ -61,21 +67,15 @@ public class JavaProcInfo extends ProcInfo {
         }
     }
 
-    // returns the java stack for the process
-    public List<String> jstack() throws Exception {
-        return run.getBuiltOn().getChannel().call(new JstackTask(""+proc.getPid()));
-    }
-
     // Keep it static inner class, otherwise JavaProcInfo needs to be
     // specified as Serializable
-    private static class JstackTask implements Callable<List<String>, Exception> {
-        private final String pid;
-
+    private static class JstackTask extends RemoteTask<List<String>> {
         JstackTask(String pid) {
-            this.pid = pid;
+            super(pid);
         }
 
         public List<String> call() throws Exception {
+            super.call();
             List<String> tiList = new ArrayList<String>();
             VirtualMachine vm = null;
             try {
@@ -114,6 +114,58 @@ public class JavaProcInfo extends ProcInfo {
                 }
             }
             return tiList;
+        }
+    }
+
+    private static class RemoteTask<T> implements Callable<T, Exception> {
+        static final Exception TOOLS_EXCEPTION;
+        static {
+            Exception te = null;
+            try {
+                addToolsJar();
+            } catch(Exception e) {
+                te = e;
+            }
+            TOOLS_EXCEPTION = te;
+        }
+        protected final String pid;
+
+        protected RemoteTask(String pid) {
+            this.pid = pid;
+        }
+
+        public T call() throws Exception {
+            if (TOOLS_EXCEPTION != null) {
+                throw TOOLS_EXCEPTION;
+            }
+            return null;
+        }
+
+        // If the slave's JVM doesn't have tools.jar in the classpath, it is added
+        // to the system class loader.
+        //
+        // URLClassLoader#addURL(URL) is called reflectively with tools.jar's URL
+        //
+        private static void addToolsJar() throws Exception {
+            ClassLoader cl = hudson.remoting.Channel.class.getClassLoader();
+            try {
+                cl.loadClass("com.sun.tools.attach.VirtualMachine");
+            } catch(ClassNotFoundException ce) {
+                if (cl instanceof URLClassLoader) {
+                    // Try to find tools.jar
+                    File jreHome = new File(System.getProperty("java.home"));
+                    File toolsJar = new File( jreHome.getParent(), "lib/tools.jar" );
+                    if (!toolsJar.exists()) {
+                        throw new RuntimeException("Cannot find tools.jar for this slave's JVM");
+                    }
+                    URL toolsURL = toolsJar.toURL();
+                    Method m = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+                    m.setAccessible(true);
+                    m.invoke(cl, toolsURL);
+                } else {
+                    throw new RuntimeException("Cannot add tools.jar to Slave's system classloader");
+                }
+            }
         }
     }
 
