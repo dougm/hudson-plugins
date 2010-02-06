7b44fa21b47acbf56d5545c9dbe339bd4fe55dd4
redsolo <redsolo@71c3de6d-444a-0410-be80-ed276b4c234a>
2010-02-06 17:29:53 +0000

    Moved out non integration test to its own test class. Added more robust retrival of the scm radio button for the plugin.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@27076 71c3de6d-444a-0410-be80-ed276b4c234a

--- googlecode/src/test/java/hudson/plugins/googlecode/scm/GoogleCodeSCMExIntegrationTest.java
+++ googlecode/src/test/java/hudson/plugins/googlecode/scm/GoogleCodeSCMExIntegrationTest.java
@@ -14,9 +14,13 @@ import org.jvnet.hudson.test.HudsonTestCase;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebAssert;
+import com.gargoylesoftware.htmlunit.html.DomNode;
 import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlVariable;
 
 public class GoogleCodeSCMExIntegrationTest extends HudsonTestCase {
 
@@ -45,7 +49,7 @@ public class GoogleCodeSCMExIntegrationTest extends HudsonTestCase {
         WebClient client = new WebClient();
         HtmlForm form = client.getPage(project, "configure").getFormByName("config");
         form.getInputByName("googlecode.googlecodeWebsite").setValueAttribute("http://code.google.com/p/leetdev3da/");
-        form.getInputsByName("scm").get(0).click(); // 
+        getGoogleCodeHtmlInput(form).click(); // 
         form.submit((HtmlButton)last(form.getHtmlElementsByTagName("button")));
         
         assertThat(project.getScm(), notNullValue());
@@ -63,7 +67,8 @@ public class GoogleCodeSCMExIntegrationTest extends HudsonTestCase {
         HtmlForm form = client.getPage(project, "configure").getFormByName("config");
         form.getInputByName("googlecode.googlecodeWebsite").setValueAttribute("http://code.google.com/p/leetdev3da/");
         form.getInputByName("googlecode.svnRemoteDirectory").setValueAttribute("tags/tag");
-        form.getInputsByName("scm").get(0).click(); // 
+        
+        getGoogleCodeHtmlInput(form).click();  
         form.submit((HtmlButton)last(form.getHtmlElementsByTagName("button")));
         
         assertThat(((GoogleCodeSCMEx) project.getScm()).getLocations()[0].getURL(), is("http://leetdev3da.googlecode.com/svn/tags/tag")); 
@@ -80,21 +85,13 @@ public class GoogleCodeSCMExIntegrationTest extends HudsonTestCase {
         HtmlForm form = client.getPage(project, "configure").getFormByName("config");
         form.getInputByName("googlecode.googlecodeWebsite").setValueAttribute("http://code.google.com/p/leetdev3da/");
         form.getInputByName("googlecode.svnRemoteDirectory").setValueAttribute("tags/tag");
-        form.getInputsByName("scm").get(0).click(); // 
+        getGoogleCodeHtmlInput(form).click(); // 
         form.submit((HtmlButton)last(form.getHtmlElementsByTagName("button")));
         
         assertEquals("SVN Directory is incorrect", "tags/tag", ((GoogleCodeSCMEx) project.getScm()).getDirectory());
         
         HtmlPage page = client.getPage(project, "configure");
         WebAssert.assertInputContainsValue(page, "googlecode.svnRemoteDirectory", "tags/tag"); 
-    }
-
-    public void testNewInstance() {
-        GoogleCodeSCMEx scm = GoogleCodeSCMEx.DescriptorImpl.newInstance(new GoogleCodeProjectProperty("http://code.google.com/p/leetdev3da/"), "trunk");
-        assertThat(scm.getLocations().length, is(1));
-        ModuleLocation location = scm.getLocations()[0];
-        assertThat(location.getLocalDir(), is("."));
-        assertThat(location.getURL(), is("http://leetdev3da.googlecode.com/svn/trunk"));
     }
     
     public void testConfiguringWithoutSettingGoogleCodeProjectUrlThrowsException() throws Exception {
@@ -104,7 +101,7 @@ public class GoogleCodeSCMExIntegrationTest extends HudsonTestCase {
             WebClient client = new WebClient();
             HtmlForm form = client.getPage(project, "configure").getFormByName("config");
             form.getInputByName("googlecode.svnRemoteDirectory").setValueAttribute("tags/tag");
-            form.getInputsByName("scm").get(0).click(); // 
+            getGoogleCodeHtmlInput(form).click(); // 
             form.submit((HtmlButton)last(form.getHtmlElementsByTagName("button")));
         } catch (FailingHttpStatusCodeException e) {            
         }
@@ -113,5 +110,19 @@ public class GoogleCodeSCMExIntegrationTest extends HudsonTestCase {
     public void testGetDescriptorReturnsCorrectDescriptor() throws Exception {
         GoogleCodeSCMEx scm = new GoogleCodeSCMEx("path2", new ModuleLocation("http://leetdev3da.googlecode.com/svn/trunk", "."));
         assertThat(scm.getDescriptor(), is(GoogleCodeSCMEx.DescriptorImpl.class));
+    }
+
+    private HtmlInput getGoogleCodeHtmlInput(HtmlForm form) {
+    	for (HtmlInput input: form.getInputsByName("scm")) {
+    		for (DomNode node : input.getParentNode().getChildren()) {
+    			if (node.getNodeName().equals("label")) {
+    				if (node.getTextContent().toLowerCase().contains("google code")) {
+    					return input;
+    				}
+    			}
+    		}
+    	}
+    	fail("No HTML input tag found for Google Code SCM");
+    	throw new RuntimeException();
     }
 }
---
+++ googlecode/src/test/java/hudson/plugins/googlecode/scm/GoogleCodeSCMExTest.java
@@ -0,0 +1,23 @@
+package hudson.plugins.googlecode.scm;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import hudson.plugins.googlecode.GoogleCodeProjectProperty;
+import hudson.scm.SubversionSCM.ModuleLocation;
+
+
+public class GoogleCodeSCMExTest {
+
+	@Test
+    public void testNewInstance() {
+        GoogleCodeSCMEx scm = GoogleCodeSCMEx.DescriptorImpl.newInstance(new GoogleCodeProjectProperty("http://code.google.com/p/leetdev3da/"), "trunk");
+        assertThat(scm.getLocations().length, is(1));
+        ModuleLocation location = scm.getLocations()[0];
+        assertThat(location.getLocalDir(), is("."));
+        assertThat(location.getURL(), is("http://leetdev3da.googlecode.com/svn/trunk"));
+    }
+
+}
