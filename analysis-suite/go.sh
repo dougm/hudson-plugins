9bfe24d0e7888b1804e96d219e03bc5e992a279f
drulli <drulli@71c3de6d-444a-0410-be80-ed276b4c234a>
2010-03-14 18:23:11 +0000

    Upgrade to Hudson 1.350.
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@28597 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ analysis-suite/go.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf $HUDSON_HOME/plugins/analysis-*
+rm -rf $HUDSON_HOME/plugins/checkstyle*
+rm -rf $HUDSON_HOME/plugins/dry*
+rm -rf $HUDSON_HOME/plugins/findbugs*
+rm -rf $HUDSON_HOME/plugins/pmd*
+rm -rf $HUDSON_HOME/plugins/tasks*
+rm -rf $HUDSON_HOME/plugins/warnings*
+
+mvn install
+
+cd ../
+cp */target/*.hpi $HUDSON_HOME/plugins
+
+
+
