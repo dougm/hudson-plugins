1b7a1299a4ba1ba70ddca661f61c0ec80ff178ae
benjaminjaton <benjaminjaton@71c3de6d-444a-0410-be80-ed276b4c234a>
2010-05-06 03:47:11 +0000

    FIXED HUDSON-3915 HUDSON-3568 HUDSON-2969
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@30767 71c3de6d-444a-0410-be80-ed276b4c234a

--- ftppublisher/src/main/java/com/zanox/hudson/plugins/FTPPublisher.java
+++ ftppublisher/src/main/java/com/zanox/hudson/plugins/FTPPublisher.java
@@ -7,6 +7,7 @@ import hudson.Util;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
+import hudson.model.Descriptor;
 import hudson.model.Result;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
@@ -24,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 
 import net.sf.json.JSONObject;
+
 import org.kohsuke.stapler.StaplerRequest;
 
 import com.jcraft.jsch.SftpException;
@@ -33,8 +35,7 @@ import com.jcraft.jsch.SftpException;
  * This class implements the ftp publisher process by using the {@link FTPSite}.
  * </p>
  * <p>
- * HeadURL: $HeadURL:
- * http://z-bld-02:8080/zxdev/zxant_test_environment/trunk/formatting/codeTemplates.xml $<br />
+ * HeadURL: $HeadURL: http://z-bld-02:8080/zxdev/zxant_test_environment/trunk/formatting/codeTemplates.xml $<br />
  * Date: $Date: 2008-04-22 11:53:34 +0200 (Di, 22 Apr 2008) $<br />
  * Revision: $Revision: 2451 $<br />
  * </p>
@@ -44,277 +45,309 @@ import com.jcraft.jsch.SftpException;
  */
 public class FTPPublisher extends Notifier {
 
-    /**
-     * Hold an instance of the Descriptor implementation of this publisher.
-     */
-    @Extension
-    public static final DescriptorImpl      DESCRIPTOR   = new DescriptorImpl();
+	/**
+	 * Hold an instance of the Descriptor implementation of this publisher.
+	 */
+	@Extension
+	public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
 
-    /**
-     * This is a SimpleDateFormat instance to get a directory name which include a time stamp.
-     */
-    protected static final SimpleDateFormat ID_FORMATTER = new SimpleDateFormat(
-                                                             "yyyy-MM-dd_HH-mm-ss");
+	/**
+	 * This is a SimpleDateFormat instance to get a directory name which include a time stamp.
+	 */
+	protected static final SimpleDateFormat ID_FORMATTER = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
 
-    private String                          siteName;
+	private String siteName;
 
-    private final List<Entry>               entries      = new ArrayList<Entry>();
+	private final List<Entry> entries = new ArrayList<Entry>();
 
-    /**
-     * The default constructor.
-     */
-    public FTPPublisher() {
-    }
+	private Boolean useTimestamps = false;
+	private Boolean flatten = true;
 
-    /**
-     * The constructor which take a configured ftp site name to publishing the artifacts.
-     * 
-     * @param siteName the name of the ftp site configuration to use
-     */
-    public FTPPublisher(String siteName) {
-        this.siteName = siteName;
-    }
+	public void setUseTimestamps(boolean useTimestamps) {
+		this.useTimestamps = useTimestamps;
+	}
 
-    /**
-     * The getter for the entries field. (this field is set by the UI part of this plugin see
-     * config.jelly file)
-     * 
-     * @return the value of the entries field
-     */
-    public List<Entry> getEntries() {
-        return entries;
-    }
+	public boolean isUseTimestamps() {
+		return useTimestamps;
+	}
 
-    /**
-     * This method returns the configured FTPSite object which match the siteName of the
-     * FTPPublisher instance. (see Manage Hudson and System Configuration point FTP)
-     * 
-     * @return the matching FTPSite or null
-     */
-    public FTPSite getSite() {
-        FTPSite[] sites = DESCRIPTOR.getSites();
-        if (siteName == null && sites.length > 0) {
-            // default
-            return sites[0];
-        }
-        for (FTPSite site : sites) {
-            if (site.getName().equals(siteName)) {
-                return site;
-            }
-        }
-        return null;
-    }
+	public void setFlatten(boolean flatten) {
+		this.flatten = flatten;
+	}
 
-    public BuildStepMonitor getRequiredMonitorService() {
-        return BuildStepMonitor.BUILD;
-    }
+	public boolean isFlatten() {
+		return flatten;
+	}
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @param build
-     * @param launcher
-     * @param listener
-     * @return
-     * @throws InterruptedException
-     * @throws IOException {@inheritDoc}
-     * @see hudson.tasks.BuildStep#perform(hudson.model.Build, hudson.Launcher,
-     *      hudson.model.BuildListener)
-     */
-    @Override
-    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
-        throws InterruptedException, IOException {
-        if (build.getResult() == Result.FAILURE) {
-            // build failed. don't post
-            return true;
-        }
+	public FTPPublisher() {
+		int a = 2;
+	}
 
-        FTPSite ftpsite = null;
-        try {
-            ftpsite = getSite();
-            listener.getLogger().println("Connecting to " + ftpsite.getHostname());
-            ftpsite.createSession();
+	/**
+	 * The constructor which take a configured ftp site name to publishing the artifacts.
+	 * 
+	 * @param siteName
+	 *          the name of the ftp site configuration to use
+	 */
+	public FTPPublisher(String siteName) {
+		this.siteName = siteName;
+	}
 
-            URI workSpaceDir = build.getWorkspace().toURI();
-            workSpaceDir = workSpaceDir.resolve(build.getProject().getName());
+	/**
+	 * The getter for the entries field. (this field is set by the UI part of this plugin see config.jelly file)
+	 * 
+	 * @return the value of the entries field
+	 */
+	public List<Entry> getEntries() {
+		return entries;
+	}
 
-            Map<String, String> envVars = build.getEnvironment(listener);
-            String targetFolder = "/" + build.getProject().getName()
-                + "/"
-                + ID_FORMATTER.format(build.getTimestamp().getTime());
-            ftpsite.mkdirs(targetFolder, listener.getLogger());
+	/**
+	 * This method returns the configured FTPSite object which match the siteName of the FTPPublisher instance. (see Manage Hudson and System
+	 * Configuration point FTP)
+	 * 
+	 * @return the matching FTPSite or null
+	 */
+	public FTPSite getSite() {
+		FTPSite[] sites = DESCRIPTOR.getSites();
+		if (siteName == null && sites.length > 0) {
+			// default
+			return sites[0];
+		}
+		for (FTPSite site : sites) {
+			if (site.getName().equals(siteName)) {
+				return site;
+			}
+		}
+		return null;
+	}
 
-            long fileCount = 0;
+	public BuildStepMonitor getRequiredMonitorService() {
+		return BuildStepMonitor.BUILD;
+	}
 
-            for (Entry e : entries) {
-                String expanded = Util.replaceMacro(e.sourceFile, envVars);
-                listener.getLogger().println(workSpaceDir);
-                FilePath[] src = build.getWorkspace().list(expanded);
-                String folderPath = Util.replaceMacro(e.filePath, envVars);
-                if (src.length == 0) {
-                    listener.getLogger().println("No file(s) found: " + expanded);
-                }
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @param build
+	 * @param launcher
+	 * @param listener
+	 * @return
+	 * @throws InterruptedException
+	 * @throws IOException
+	 *           {@inheritDoc}
+	 * @see hudson.tasks.BuildStep#perform(hudson.model.Build, hudson.Launcher, hudson.model.BuildListener)
+	 */
+	@Override
+	public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+		if (build.getResult() == Result.FAILURE || build.getResult() == Result.ABORTED) {
+			// build failed. don't post
+			return true;
+		}
 
-                if (src.length == 1) {
-                    ftpsite.mkdirs(folderPath, listener.getLogger());
-                    ftpsite.upload(src[0], envVars, listener.getLogger());
-                    fileCount++;
-                } else {
-                    String previosePath = "";
-                    for (FilePath s : src) {
+		FTPSite ftpsite = null;
+		try {
+			ftpsite = getSite();
+			listener.getLogger().println("Connecting to " + ftpsite.getHostname());
+			ftpsite.createSession();
 
-                        URI relativePath = workSpaceDir.relativize(s.toURI());
-                        String relativePathString = relativePath.toString().substring(0,
-                            relativePath.toString().lastIndexOf('/'));
-                        if (!previosePath.equals(relativePathString)) {
-                            ftpsite.changedToProjectRootDir(targetFolder, listener.getLogger());
-                            ftpsite.mkdirs(relativePathString, listener.getLogger());
-                            previosePath = relativePathString;
-                        }
-                        ftpsite.upload(s, envVars, listener.getLogger());
-                        fileCount++;
-                    }
-                }
-                ftpsite.changedToProjectRootDir(targetFolder, listener.getLogger());
-            }
-            listener.getLogger().println("transferred " + fileCount + " files");
-        } catch (IOException e) {
-            e.printStackTrace(listener.error("Failed to upload files"));
-            build.setResult(Result.UNSTABLE);
-        } catch (SftpException e) {
-            e.printStackTrace();
-        } finally {
-            if (ftpsite != null) {
-                ftpsite.closeSession();
-            }
-        }
+			URI workSpaceDir = build.getWorkspace().toURI().normalize();
+			// workSpaceDir = workSpaceDir.resolve(build.getProject().getName()).normalize();
 
-        return true;
-    }
+			Map<String, String> envVars = build.getEnvironment(listener);
 
-    /**
-     * <p>
-     * This class holds the metadata for the FTPPublisher.
-     * </p>
-     * 
-     * @author $Author: ZANOX-COM\fit $
-     * @see Descriptor
-     */
-    public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+			long fileCount = 0;
 
-        private final CopyOnWriteList<FTPSite> sites = new CopyOnWriteList<FTPSite>();
+			for (Entry e : entries) {
+				// prepare sources
+				String expanded = Util.replaceMacro(e.sourceFile, envVars);
+				listener.getLogger().println(workSpaceDir);
+				FilePath[] sourceFiles = build.getWorkspace().list(expanded);
 
-        /**
-         * The default constructor.
-         */
-        public DescriptorImpl() {
-            super(FTPPublisher.class);
-            load();
-        }
+				// prepare common dest
+				ftpsite.changedToProjectRootDir("", listener.getLogger());
+				String subRoot = Util.replaceMacro(e.filePath, envVars);
+				if (useTimestamps) {
+					subRoot += "/" + ID_FORMATTER.format(build.getTimestamp().getTime());
+				}
+				ftpsite.mkdirs(subRoot, listener.getLogger());
+				ftpCurrentDirectory = subRoot;
 
-        /**
-         * The name of the plugin to display them on the project configuration web page.
-         * 
-         * {@inheritDoc}
-         * 
-         * @return {@inheritDoc}
-         * @see hudson.model.Descriptor#getDisplayName()
-         */
-        @Override
-        public String getDisplayName() {
-            return "Publish artifacts to FTP";
-        }
+				if (sourceFiles.length == 0) {
+					listener.getLogger().println("No file(s) found: " + expanded);
+				} else {
+					for (FilePath sourceFile : sourceFiles) {
+						ftpCdMkdirs(sourceFile, workSpaceDir, ftpsite, subRoot, listener);
+						ftpsite.upload(sourceFile, envVars, listener.getLogger());
+						fileCount++;
+					}
+				}
+				listener.getLogger().println("transferred " + fileCount + " files to " + subRoot);
+			}
+		} catch (Throwable th) {
+			th.printStackTrace(listener.error("Failed to upload files"));
+			build.setResult(Result.UNSTABLE);
+		} finally {
+			if (ftpsite != null) {
+				ftpsite.closeSession();
+			}
+		}
 
-        /**
-         * Return the location of the help document for this publisher.
-         * 
-         * {@inheritDoc}
-         * 
-         * @return {@inheritDoc}
-         * @see hudson.model.Descriptor#getHelpFile()
-         */
-        @Override
-        public String getHelpFile() {
-            return "/plugin/ftppublisher/help.html";
-        }
+		return true;
+	}
 
-        @Override
-        public boolean isApplicable(Class<? extends AbstractProject> jobType) {
-            return true;
-        }
+	private String ftpCurrentDirectory;
 
-        /**
-         * This method is called by hudson if the user has clicked the add button of the FTP
-         * repository hosts point in the System Configuration web page. It's create a new instance
-         * of the {@link FTPPublisher} class and added all configured ftp sites to this instance by
-         * calling the method {@link FTPPublisher#getEntries()} and on it's return value the addAll
-         * method is called.
-         * 
-         * {@inheritDoc}
-         * 
-         * @param req {@inheritDoc}
-         * @return {@inheritDoc}
-         * @see hudson.model.Descriptor#newInstance(org.kohsuke.stapler.StaplerRequest)
-         */
-        @Override
-        public Publisher newInstance(StaplerRequest req, JSONObject formData) {
-            FTPPublisher pub = new FTPPublisher();
-            req.bindParameters(pub, "ftp.");
-            pub.getEntries().addAll(req.bindParametersToList(Entry.class, "ftp.entry."));
-            return pub;
-        }
+	private void ftpCdMkdirs(FilePath sourceFile, URI workSpaceDirURI, FTPSite ftpsite, String entryRootFolder, BuildListener listener)
+	    throws IOException, SftpException, InterruptedException {
+		String relativeSourcePath = getRelativeSourcePath(workSpaceDirURI, sourceFile);
+		if (!ftpCurrentDirectory.equals(relativeSourcePath)) {
+			ftpsite.changedToProjectRootDir(entryRootFolder, listener.getLogger());
+			ftpCurrentDirectory = entryRootFolder;
+			if (!flatten) {
+				ftpsite.mkdirs(relativeSourcePath, listener.getLogger());
+				ftpCurrentDirectory += relativeSourcePath;
+			}
+		}
+	}
 
-        /**
-         * The getter of the sites field.
-         * 
-         * @return the value of the sites field.
-         */
-        public FTPSite[] getSites() {
-            Iterator<FTPSite> it = sites.iterator();
-            int size = 0;
-            while (it.hasNext()) {
-                it.next();
-                size++;
-            }
-            return sites.toArray(new FTPSite[size]);
-        }
+	private String getRelativeSourcePath(URI workSpaceDirURI, FilePath sourceFile) throws IOException, InterruptedException {
+		URI sourceFileURI = sourceFile.toURI().normalize();
+		String relativeSourceFile = sourceFileURI.getPath().replaceFirst(workSpaceDirURI.getPath(), "");
+		int lastSlashIndex = relativeSourceFile.lastIndexOf("/");
+		if (lastSlashIndex == -1) {
+			return ".";
+		} else {
+			return relativeSourceFile.substring(0, lastSlashIndex);
+		}
+	}
 
-        /**
-         * {@inheritDoc}
-         * 
-         * @param req {@inheritDoc}
-         * @return {@inheritDoc}
-         * @see hudson.model.Descriptor#configure(org.kohsuke.stapler.StaplerRequest)
-         */
-        @Override
-        public boolean configure(StaplerRequest req, JSONObject formData) {
-            sites.replaceBy(req.bindParametersToList(FTPSite.class, "ftp."));
-            save();
-            return true;
-        }
+	/**
+	 * <p>
+	 * This class holds the metadata for the FTPPublisher.
+	 * </p>
+	 * 
+	 * @author $Author: ZANOX-COM\fit $
+	 * @see Descriptor
+	 */
+	public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
 
-        /**
-         * This method validates the current entered ftp configuration data. That is made by create
-         * a ftp connection.
-         * 
-         * @param request the current {@link javax.servlet.http.HttpServletRequest}
-         */
-        public FormValidation doLoginCheck(StaplerRequest request) {
-            String hostname = Util.fixEmpty(request.getParameter("hostname"));
-            if (hostname == null) { // hosts is not entered yet
-                return FormValidation.ok();
-            }
-            FTPSite site = new FTPSite(hostname, request.getParameter("port"), request
-                .getParameter("timeOut"), request.getParameter("user"), request
-                .getParameter("pass"));
-            try {
-                site.createSession();
-                site.closeSession();
+		private final CopyOnWriteList<FTPSite> sites = new CopyOnWriteList<FTPSite>();
 
-                return FormValidation.ok();
-            } catch (Exception e) {
-                return FormValidation.error(e.getMessage());
-            }
-        }
-    }
+		/**
+		 * The default constructor.
+		 */
+		public DescriptorImpl() {
+			super(FTPPublisher.class);
+			load();
+		}
+
+		/**
+		 * The name of the plugin to display them on the project configuration web page.
+		 * 
+		 * {@inheritDoc}
+		 * 
+		 * @return {@inheritDoc}
+		 * @see hudson.model.Descriptor#getDisplayName()
+		 */
+		@Override
+		public String getDisplayName() {
+			return "Publish artifacts to FTP";
+		}
+
+		/**
+		 * Return the location of the help document for this publisher.
+		 * 
+		 * {@inheritDoc}
+		 * 
+		 * @return {@inheritDoc}
+		 * @see hudson.model.Descriptor#getHelpFile()
+		 */
+		@Override
+		public String getHelpFile() {
+			return "/plugin/ftppublisher/help.html";
+		}
+
+		@Override
+		public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+			return true;
+		}
+
+		/**
+		 * This method is called by hudson if the user has clicked the add button of the FTP repository hosts point in the System Configuration
+		 * web page. It's create a new instance of the {@link FTPPublisher} class and added all configured ftp sites to this instance by calling
+		 * the method {@link FTPPublisher#getEntries()} and on it's return value the addAll method is called.
+		 * 
+		 * {@inheritDoc}
+		 * 
+		 * @param req
+		 *          {@inheritDoc}
+		 * @return {@inheritDoc}
+		 * @see hudson.model.Descriptor#newInstance(org.kohsuke.stapler.StaplerRequest)
+		 */
+		@Override
+		public Publisher newInstance(StaplerRequest req, JSONObject formData) {
+			FTPPublisher pub = new FTPPublisher();
+			pub.setFlatten(formData.getBoolean("flatten"));
+			pub.setUseTimestamps(formData.getBoolean("useTimestamps"));
+			req.bindParameters(pub, "publisher.");
+			req.bindParameters(pub, "ftp.");
+			pub.getEntries().addAll(req.bindParametersToList(Entry.class, "ftp.entry."));
+			return pub;
+		}
+
+		/**
+		 * The getter of the sites field.
+		 * 
+		 * @return the value of the sites field.
+		 */
+		public FTPSite[] getSites() {
+			Iterator<FTPSite> it = sites.iterator();
+			int size = 0;
+			while (it.hasNext()) {
+				it.next();
+				size++;
+			}
+			return sites.toArray(new FTPSite[size]);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 * 
+		 * @param req
+		 *          {@inheritDoc}
+		 * @return {@inheritDoc}
+		 * @see hudson.model.Descriptor#configure(org.kohsuke.stapler.StaplerRequest)
+		 */
+		@Override
+		public boolean configure(StaplerRequest req, JSONObject formData) {
+			sites.replaceBy(req.bindParametersToList(FTPSite.class, "ftp."));
+			save();
+			return true;
+		}
+
+		/**
+		 * This method validates the current entered ftp configuration data. That is made by create a ftp connection.
+		 * 
+		 * @param request
+		 *          the current {@link javax.servlet.http.HttpServletRequest}
+		 */
+		public FormValidation doLoginCheck(StaplerRequest request) {
+			String hostname = Util.fixEmpty(request.getParameter("hostname"));
+			if (hostname == null) { // hosts is not entered yet
+				return FormValidation.ok();
+			}
+			FTPSite site = new FTPSite(hostname, request.getParameter("port"), request.getParameter("timeOut"), request.getParameter("user"),
+			    request.getParameter("pass"));
+			site.setFtpDir(request.getParameter("ftpDir"));
+			try {
+				site.createSession();
+				site.closeSession();
+
+				return FormValidation.ok();
+			} catch (Exception e) {
+				return FormValidation.error(e.getMessage());
+			}
+		}
+	}
 }
