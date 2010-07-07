4b87d40f9b8f1337cc2a064bda48a81779b00445
gbois <gbois@71c3de6d-444a-0410-be80-ed276b4c234a>
2010-07-07 10:51:17 +0000

    Fixed [HUDSON-5791]
    git-svn-id: https://hudson.dev.java.net/svn/hudson/trunk/hudson/plugins@32545 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ scons/src/main/java/com/thalesgroup/hudson/plugins/scons/SConsBuilder.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Thales Corporate Services SAS                             *
+ * Author : Gregory Boissinot                                                   *
+ *                                                                              *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy *
+ * of this software and associated documentation files (the "Software"), to deal*
+ * in the Software without restriction, including without limitation the rights *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell    *
+ * copies of the Software, and to permit persons to whom the Software is        *
+ * furnished to do so, subject to the following conditions:                     *
+ *                                                                              *
+ * The above copyright notice and this permission notice shall be included in   *
+ * all copies or substantial portions of the Software.                          *
+ *                                                                              *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,*
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN    *
+ * THE SOFTWARE.                                                                *
+ *******************************************************************************/
+
+package com.thalesgroup.hudson.plugins.scons;
+
+import hudson.tasks.Builder;
+
+
+@SuppressWarnings("unused")
+public class SConsBuilder extends Builder {
+
+    private String sconsName;
+
+    private String options;
+
+    private String variables;
+
+    private String targets;
+
+    private String rootSconsscriptDirectory;
+
+    private String sconsscript;
+
+    @SuppressWarnings("unused")
+    public Object readResolve() {
+        return new SConsBuilderScriptFile(this.sconsName, this.options, this.variables, this.targets, this.rootSconsscriptDirectory, this.sconsscript);
+    }
+
+}
